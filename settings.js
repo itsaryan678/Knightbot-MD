fd3e8476bde8d23979e83168f3cbaf8d84f983a9
Itz Aryan <163823860+itsaryan678@users.noreply.github.com>
2025-05-23 09:29:08 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'Knight Bot',
   author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botName: "Orochi BestBot 🤖",
+  botOwner: 'Aryan Chauhan', // Your name
+  ownerNumber: '919317171638', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
